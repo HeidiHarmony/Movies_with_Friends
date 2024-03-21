893e2dc9d94299813c6cf6f78139094667849840
HeidiHarmony <tenibrae@gmail.com>
2024-03-21 08:47:14 -0400

    analyzing directory tree, deleting unneccesary files and creating ones that will be used. fixing naming conventions for consistency.

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,8 @@
 const router = require('express').Router();
-const express = require('express');
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Other routes using the default layout
-app.get('/main', (req, res) => {
-    res.render('main', { layout: 'main' }); // Will use the default 'main' layout
-});
 
 module.exports = router;
